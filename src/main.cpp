ae6378e302f0d7a11cbaab6b7d35d09e2943afe9
lemon857 <lemon857.an@gmail.com>
2024-07-29 16:45:07 +0500

    add: log in action

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char **argv)
     size_t resultHash;
     int action;
 
-    std::cout << "Select registration or sing in (0/1): \n";
+    std::cout << "Select action (sing up(0) or log in(1)): \n";
     std::cin >> action;
     std::cout << "Enter your login: ";
     std::cin >> login;
@@ -23,7 +23,7 @@ int main(int argc, char **argv)
     {
         
         std::ofstream f;
-        f.open("base.txt");
+        f.open("base.txt", std::ios::app);
         if (!f.is_open()) 
         {  
             std::cerr << "Error open file for write\n";
@@ -34,7 +34,28 @@ int main(int argc, char **argv)
     }
     else 
     {
-
+        std::ifstream f;
+        f.open("base.txt");
+        if (!f.is_open()) 
+        {  
+            std::cerr << "Error open file for read\n";
+            return -1;
+        }
+        std::string tmpLogin, tmpHash;
+        while (tmpLogin != login && !f.eof())
+        {
+            f >> tmpLogin;
+            f >> tmpHash;
+        }
+        if (tmpLogin == login && std::atoll(tmpHash.c_str()) == resultHash)
+        {
+            std::cout << "You're welcome!\n";
+        }
+        else 
+        {
+            std::cout << "Invalid login or password\n";
+        }
+        f.close();
     }
 
     return 0;
